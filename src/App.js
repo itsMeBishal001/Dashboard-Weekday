0323b2e5703c196d85444e2dcdc70a8b29676a7b
BISHAL DATTA <dattabishal001@gmail.com>
2024-05-05 13:27:21 +0530

    jobs data initial fetch is done and data is printing on console

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
-  return <div>Now ready to start initial setup ready </div>;
+  const [jobs, setJobs] = useState(null);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch(
+          "https://api.weekday.technology/adhoc/getSampleJdJSON",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              limit: 10,
+              offset: 0,
+            }),
+          }
+        );
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error("Fetch error:", error);
+        return null;
+      }
+    };
+
+    // Fetch initial data
+    fetchJobs().then((data) => {
+      if (data) {
+        setJobs(data);
+      }
+    });
+  }, []);
+
+  //jobs data results
+  console.log(jobs);
+
+  return (
+    <div>
+      <h1>jobs Data</h1>
+      {jobs ? <>Jobs are fetched </> : <p>Loading...</p>}
+    </div>
+  );
 }
 
 export default App;
